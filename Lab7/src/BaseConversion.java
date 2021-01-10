54a37d1641a1690ce53e7e63bc065dff2119c722
Alex Wells <Awells23@emich.edu>
2021-01-10 15:31:39 -0500

    First Commit

---
+++ Lab7/src/BaseConversion.java
@@ -0,0 +1,28 @@
+// ****************************************************************** 
+//   BaseConversion.java 
+// 
+//   Recursively converts an integer from base 10 to another base 
+// ****************************************************************** 
+ 
+import java.util.Scanner; 
+ 
+public class BaseConversion 
+{ 
+    public static void main (String[] args) 
+    { 
+		 int base10Num; 
+		 int base; 
+		 
+		 Scanner scan = new Scanner(System.in); 
+		 
+		 System.out.println (); 
+		 System.out.println ("Base Conversion Program"); 
+		 System.out.print ("Enter an integer: "); 
+		 base10Num = scan.nextInt(); 
+		 System.out.print ("Enter the base: "); 
+		 base = scan.nextInt(); 
+		 
+		 // Call convert and print the answer 
+		  
+    } 
+}
